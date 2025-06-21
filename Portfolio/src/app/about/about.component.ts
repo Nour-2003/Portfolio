100e431b2ad74580754aa51ad26fc2737e5308cf
Nour-2003 <noureldeanh@gmail.com>
2025-06-21 22:03:28 +0300

    About Section Done

--- Portfolio/src/app/about/about.component.ts
+++ Portfolio/src/app/about/about.component.ts
@@ -1,12 +1,27 @@
-import { Component, HostListener } from '@angular/core';
+import { NgFor } from "@angular/common";
+import { Component, HostListener, NgModule } from "@angular/core";
+import { EducationComponent } from "../education/education.component";
+import { CourseComponent } from "../course/course.component";
+import { ExperienceComponent } from "../experience/experience.component";
+import { DetailsComponent } from "../details/details.component";
 
 @Component({
-  selector: 'app-about',
+  selector: "app-about",
   standalone: true,
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  imports: [NgFor, EducationComponent, CourseComponent, ExperienceComponent, DetailsComponent],
+  templateUrl: "./about.component.html",
+  styleUrl: "./about.component.css",
 })
 export class AboutComponent {
- 
+  navItems = [
+    "Education Qualification",
+    "Short Course",
+    "Experience",
+    "Details",
+  ];
+  selectedIndex = 0;
+
+  selectItem(index: number) {
+    this.selectedIndex = index;
+  }
 }
